d7f9eb22b9f1aa4b153626f60d3d7a73b0e32b81
Haiyoooo <38852079+Haiyoooo@users.noreply.github.com>
2018-09-28 14:06:32 -0700

    Main code

---
+++ HelloWorldUCSC/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello, World!" << endl;
+	cout << "Yay!!! 5 bonus points, thanks Walt :D" << endl;
+	system("pause");
+	return 0; // arbitary number but by convention, return success = 0.
+}
